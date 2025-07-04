59f9f1c5e6af1ba53c566462d547c91fb55a6d75
Behn Hayhoe <7383025+BehnH@users.noreply.github.com>
2025-07-04 07:22:09 +0100

    Use undici directly & enable http/2

--- src/run/octosts-action.ts
+++ src/run/octosts-action.ts
@@ -2,6 +2,7 @@ import * as crypto from "node:crypto";
 import * as core from "@actions/core"
 import * as httm from "@actions/http-client"
 import {getActionsEnvVars, getInputs} from "./inputs";
+import {Agent, fetch, setGlobalDispatcher} from "undici";
 
 interface GHRep {
     value: string;
@@ -14,38 +15,44 @@ interface OctoStsRep {
 
 export async function run(): Promise<void> {
     try {
+        const agent = new Agent({ allowH2: true })
+        setGlobalDispatcher(agent)
         const {actionsToken, actionsUrl} = getActionsEnvVars();
         const { domain, scope, identity } = getInputs();
 
-        const httpClient = new httm.HttpClient('octosts-action')
 
-        const ghRep = await httpClient.getJson<GHRep>(
-            `${actionsUrl}&audience=${domain}`, {
-            authorization: `Bearer ${actionsToken}`,
+        const ghRep = await fetch(`${actionsUrl}&audience=${domain}`, {
+            headers: {
+                authorization: `Bearer ${actionsToken}`,
+            }
         })
-        if (ghRep.result?.value !== null) {
-            core.setSecret(ghRep.result!.value)
+        const ghRepJson = await ghRep.json() as GHRep
+        if (ghRepJson.value !== null) {
+            core.setSecret(ghRepJson.value)
         }
-        core.debug(JSON.stringify(ghRep))
+        core.debug(JSON.stringify(ghRepJson))
 
-        const octoStsRep = await httpClient.getJson<OctoStsRep>(
+        const octoStsRep = await fetch(
             `https://${domain}/sts/exchange?scope${scope}&identity=${identity}`, {
-            authorization: `Bearer ${ghRep.result?.value}`
+            headers: {
+                authorization: `Bearer ${ghRepJson.value}`
+            }
         })
+        const octoStsRepJson = await octoStsRep.json() as OctoStsRep
 
-        if (!octoStsRep.result?.token) {
-            core.setFailed(octoStsRep.result?.message as string)
+        if (!octoStsRepJson?.token) {
+            core.setFailed(octoStsRepJson?.message as string)
         }
 
         const tokHash = crypto
             .createHash('sha256')
-            .update(octoStsRep.result?.token as string)
+            .update(octoStsRepJson?.token as string)
             .digest('hex')
         core.info(`Created token with hash: ${tokHash}`)
 
-        core.setSecret(octoStsRep.result?.token as string)
-        core.setOutput('token', octoStsRep.result?.token)
-        core.saveState('token', octoStsRep.result?.token)
+        core.setSecret(octoStsRepJson?.token as string)
+        core.setOutput('token', octoStsRepJson?.token)
+        core.saveState('token', octoStsRepJson?.token)
     } catch (error) {
         core.debug(JSON.stringify(error))
         core.setFailed((error as Error).message)
